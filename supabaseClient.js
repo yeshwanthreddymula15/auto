74c350c2de2be7f811b9fdc98d3be0b2890c0252
yeshreddy15 <yeshwanthreddymula@gmail.com>
2025-06-07 22:23:33 +0530

    Initial commit

---
+++ supabaseClient.js
@@ -0,0 +1,7 @@
+// supabaseClient.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://cmsfimcmstkwuzkbzhoz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtc2ZpbWNtc3Rrd3V6a2J6aG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDQwNjEsImV4cCI6MjA2NDg4MDA2MX0.-Gzlr-tdR1KYrXiJhAZZPmbhtMHmcTytCAZE3qtB7cE'
+);
---
+++ tests/playwright.config.js
@@ -0,0 +1,6 @@
+// playwright.config.js
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',  // Make sure this points to your tests folder
+});
